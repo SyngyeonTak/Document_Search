17925c5588947b7f2975b03fc8cb1616de9ae146
sytag <a01055356349@gmail.com>
2026-04-24 16:29:30 +0900

    hybrid modi

--- search/retriever/hybrid.py
+++ search/retriever/hybrid.py
@@ -11,44 +11,44 @@ class HybridRetriever:
         self.vector_retriever = vector_retriever
         self.alpha = alpha
 
-        def search(self, request):
-            bm25_results = self.bm25_retriever.search(
-                type(request)(
-                    query=request.query,
-                    top_k=request.top_k * 3,
-                    filters=request.filters,
-                )
+    def search(self, request):
+        bm25_results = self.bm25_retriever.search(
+            type(request)(
+                query=request.query,
+                top_k=request.top_k * 3,
+                filters=request.filters,
             )
-            vector_results = self.vector_retriever.search(
-                type(request)(
-                    query=request.query,
-                    top_k=request.top_k * 3,
-                    filters=request.filters,
-                )
+        )
+        vector_results = self.vector_retriever.search(
+            type(request)(
+                query=request.query,
+                top_k=request.top_k * 3,
+                filters=request.filters,
             )
+        )
 
-            score_map = {}
-            result_map = {}
+        score_map = {}
+        result_map = {}
 
-            for rank, item in enumerate(bm25_results, start=1):
-                uid= item.unique_id
+        for rank, item in enumerate(bm25_results, start=1):
+            uid= item.unique_id
+            result_map[uid] = deepcopy(item)
+            score_map[uid] = score_map.get(uid, 0.0) + self.alpha * rrf_score(rank)
+
+        for rank, item in enumerate(vector_results, start=1):
+            uid = item.unique_id
+            if uid not in result_map:
                 result_map[uid] = deepcopy(item)
-                score_map[uid] = score_map.get(uid, 0.0) + self.alpha * rrf_score(rank)
+            score_map[uid] = score_map.get(uid, 0.0) + (1 - self.alpha) * rrf_score(rank)
 
-            for rank, item in enumerate(vector_results, start=1):
-                uid = item.unique_id
-                if uid not in result_map:
-                    result_map[uid] = deepcopy(item)
-                score_map[uid] = score_map.get(uid, 0.0) + (1 - self.alpha) * rrf_score(rank)
+        ranked = sorted(score_map.items(), key=lambda x: x[1], reverse=True)[:request.top_k]
 
-            ranked = sorted(score_map.items(), key=lambda x: x[1], reverse=True)[:request.top_k]
+        fused = []
+        for uid, score in ranked:
+            item = result_map[uid]
+            item.score = score
+            item.raw_score = score
+            item.retrieval_method = self.name
+            fused.append(item)
 
-            fused = []
-            for uid, score in ranked:
-                item = result_map[uid]
-                item.score = score
-                item.raw_score = score
-                item.retrieval_method = self.name
-                fused.append(item)
-
-            return fused                
+        return fused                
